b9137f625ad73185ab156f48ba3799ad2fc1b203
Josh Gillespie <josh@bethel.io>
2017-03-11 20:09:18 -0600

    Add day get and post routes

--- app/routes.js
+++ app/routes.js
@@ -1,12 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+// Models
+var Day = require('./models/day');
+
+// Routes
 router.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
 router.get('/days', function(req, res) {
-  res.send('Get all days');
+  Day.find().exec(function(err, days) {
+    if (err) {
+      res.status(400).send({ error: err});
+    }
+    res.status(200).send({ days: days });
+  });
+});
+
+router.post('/days', function(req, res) {
+  console.log('days post');
+  Day.create({}, function(err, day) {
+    if (err) {
+      res.status(400).send( {error: err });
+    }
+    res.status(201).send(day);
+  });
 });
 
 module.exports = router;
