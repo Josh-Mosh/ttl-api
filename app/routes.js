952a3fb7a9194da3c2fab8f10beabf08aebf727e
Josh Gillespie <josh@bethel.io>
2017-06-04 14:36:02 -0500

    add delete days/:id route

--- app/routes.js
+++ app/routes.js
@@ -6,13 +6,13 @@ var Day = require('./models/day');
 
 // Routes
 router.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Hello Travelers!');
 });
 
 router.get('/days', function(req, res) {
   Day.find().exec(function(err, days) {
     if (err) {
-      res.status(400).send({ error: err});
+      res.status(400).send({ error: err });
     }
     res.status(200).send({ days: days });
   });
@@ -30,9 +30,19 @@ router.post('/days', function(req, res) {
 
   Day.create(day, function(err, day) {
     if (err) {
-      res.status(400).send( {error: err });
+      res.status(400).send({ error: err });
     }
     res.status(201).send({ day: day });
+  });
+});
+
+router.delete('/days/:id', function(req, res) {
+  console.log('delete days');
+  Day.findByIdAndRemove(req.params.id, function(err, day) {
+    if (err) {
+      res.status(400).send({ error: err });
+    }
+    res.status(200).send({ day: day });
   });
 });
 
